495ec9c826860213f831cae9a0c95547d2a4ca0e
lzhang91 <73509620+lzhang91@users.noreply.github.com>
2023-09-28 10:19:34 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal-Portfolio-Website
+Created a personal portfolio website using HTML, CSS, and JavaScript for my Advanced Application Development class. Also, integrated mobile, tablet, and desktop viewports. After completion of the website, I presented the website to colleagues and professors. 
---
+++ scripts.js
@@ -0,0 +1,25 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
